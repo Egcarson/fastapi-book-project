3cb57c2a089f7d5bcf936c8f2c68ca117923bc2a
prevail <esehgodprevail@gmail.com>
2025-02-12 21:53:51 +0100

    Remove delete_book endpoint to test automatic deployment

--- tests/test_books.py
+++ tests/test_books.py
@@ -7,14 +7,14 @@ def test_get_all_books():
     assert len(response.json()) == 3
 
 
-# def test_get_single_book():
-#     response = client.get("/books/1")
-#     assert response.status_code == 200
-#     data = response.json()
-#     assert data["title"] == "The Hobbit"
-#     assert data["author"] == "J.R.R. Tolkien"
-#     response = client.get("/books/9")
-#     assert response.status_code == 404
+def test_get_single_book():
+    response = client.get("/books/1")
+    assert response.status_code == 200
+    data = response.json()
+    assert data["title"] == "The Hobbit"
+    assert data["author"] == "J.R.R. Tolkien"
+    response = client.get("/books/9")
+    assert response.status_code == 404
 
 
 def test_create_book():
@@ -46,9 +46,9 @@ def test_update_book():
     assert data["title"] == "The Hobbit: An Unexpected Journey"
 
 
-def test_delete_book():
-    response = client.delete("/books/3")
-    assert response.status_code == 204
+# def test_delete_book():
+#     response = client.delete("/books/3")
+#     assert response.status_code == 204
 
-    response = client.get("/books/3")
-    assert response.status_code == 404
+#     response = client.get("/books/3")
+#     assert response.status_code == 404
